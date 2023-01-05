7b9d45a5f87e44dd1f664ac3c27836828dc1f5ef
Rahul Sharma <rahulsrma26@gmail.com>
2023-01-05 23:17:27 +0000

    :heavy_check_mark: added check for zipfile

--- committer.sh
+++ committer.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 TIMESTAMP=$(date -d "today" +"%Y%m%d-%H%M%S")
 OUTDIR=${2:-'./tmp/'}
 OUTFILE="${OUTDIR}${TIMESTAMP}"
 git --no-pager diff --staged --name-only | zip $OUTFILE.zip -@
-git rev-parse --abbrev-ref HEAD >> $OUTFILE.txt
-echo $1 >> $OUTFILE.txt
+if test -f "$OUTFILE.zip"; then
+    git config --get remote.origin.url > $OUTFILE.txt
+    git rev-parse --abbrev-ref HEAD >> $OUTFILE.txt
+    echo $1 >> $OUTFILE.txt
+fi
