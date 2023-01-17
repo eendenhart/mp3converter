d2b3985fa145400ffc566f09a23a22c63373c932
Rahul Sharma <rahulsrma26@gmail.com>
2023-01-17 01:56:43 +0000

    :package: archiver init

---
+++ archiver/archive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IN_DIR=./tmp/original
+OUT_DIR=./tmp/converted
+
+for f in "$IN_DIR"/* ; do
+    if [ -d "$f" ]; then
+        name=$(basename "$f")
+        zip -r "$OUT_DIR/$name".zip "$f"
+    fi
+done
