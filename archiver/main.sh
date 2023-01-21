29ff379017da1fe7cb2481e58c4a15f809fd7315
Rahul Sharma <rahulsrma26@gmail.com>
2023-01-21 16:47:00 +0000

    :recycle: minor refactoring

--- archiver/main.sh
+++ archiver/main.sh
@@ -5,20 +5,19 @@ gpg --trust-model always --import "$IMPORT_FILE"
 for f in "$IN_DIR"/* ; do
     if [ -d "$f" ]; then
         name=$(basename "$f")
-        rebuild=false
+        rebuild=true
         if [[ -e "$OUT_DIR/$name.zip.gpg" ]]; then
             newest="$(find $f -type f -print0 | xargs -0 ls -drt | tail -n 1)"
             if [ "$newest" -nt "$OUT_DIR/$name.zip.gpg" ]; then
                 echo "Newest modified $newest"
-                rebuild=true
             else
                 echo "Skipping $name"
+                rebuild=false
             fi
-        else
-            rebuild=true
         fi
         if [ "$rebuild" = true ]; then
             zip -r -q "$OUT_DIR/$name".zip "$f"
+            echo "Encrypting $name"
             gpg -e --recipient "$EMAIL" --yes --trust-model always "$OUT_DIR/$name".zip
             rm "$OUT_DIR/$name".zip
         fi
