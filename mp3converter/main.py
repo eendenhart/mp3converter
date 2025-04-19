ea5ec0e4d5ceb03d1b873835be7dbb450ee82055
eendenhart <32901240+eendenhart@users.noreply.github.com>
2025-04-19 12:39:11 +0200

    Update main.py

--- mp3converter/main.py
+++ mp3converter/main.py
@@ -8,7 +8,7 @@ import subprocess
 import shutil
 from glob import glob
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 def get_args(args: list = None):
     parser = argparse.ArgumentParser(
@@ -17,6 +17,7 @@ def get_args(args: list = None):
         description=__doc__,
     )
     parser.add_argument("input", type=str, help="input dir")
+    parser.add_argument("output", type=str, help="output dir (can be same as input)")
     parser.add_argument(
         "-e",
         "--extensions",
@@ -27,7 +28,6 @@ def get_args(args: list = None):
     return parser.parse_args(args) if args else parser.parse_args()
 
 def encode_mp3(i_path: str, o_path: str, quality: int):
-    """Encode given audio file to mp3 using ffmpeg command."""
     prog = "ffmpeg"
     if "win" in platform.system().lower():
         prog += ".exe"
@@ -42,41 +42,15 @@ def encode_mp3(i_path: str, o_path: str, quality: int):
         o_path,
     ]
     print(" ".join(cmd))
-    subprocess.run(cmd)
+    subprocess.run(cmd, check=False)
 
 def main(args):
-    exts = set(args.extensions.lower().split(";"))  # alleen opgegeven extensies
-    filter_func = lambda p: os.path.splitext(p.lower())[-1] in exts
+    exts = set(args.extensions.split(";"))
+    exts.add(".mp3")  # om bestaande mp3's te detecteren
     paths = glob(f"{args.input}/**/*.*", recursive=True)
-    paths = sorted(list(filter(filter_func, paths)))
+    paths = sorted(p for p in paths if os.path.splitext(p.lower())[-1] in exts)
+
     processed = 0
     for i_path in paths:
         i_path = os.path.abspath(i_path)
-        o_path = os.path.splitext(i_path)[0] + ".mp3"
-
-        # Skip als output mp3 recenter is dan originele bestand
-        if os.path.isfile(o_path):
-            if os.path.getmtime(o_path) > os.path.getmtime(i_path):
-                continue  # up-to-date
-            os.remove(o_path)
-
-        # Zorg dat map bestaat
-        os.makedirs(os.path.dirname(o_path), exist_ok=True)
-        encode_mp3(i_path, o_path, 192)
-        print(f"{i_path} >> {o_path}")
-        processed += 1
-
-    if not processed:
-        print("Everything was up-to-date.")
-
-if __name__ == "__main__":
-    print(f"{VERSION=}")
-    src = os.environ.get("MP3CONVERTER_SRC", None)
-    if src:
-        args = [src]
-        if ext := os.environ.get("MP3CONVERTER_EXT", None):
-            args.extend(["-e", ext])
-        print(f"{args=}")
-        main(get_args(args))
-    else:
-        main(get_args())
+        o_path = i_path.replace(args.input,
